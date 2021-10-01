1702e2c4932963292e7b83f33d03e50e35e7a7aa
Rodent Control <rdnt.cntrl@localhost>
2021-10-01 00:00:00 +0000

    try making the build reproducible

---
+++ caca_deterministic_zip.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+# ensure sort is consistent
+export LC_ALL=C
+
+if [ "$#" -lt 3 ]; then
+	echo "missing arguments" >&2
+	exit 1
+fi
+
+TIMESTAMP="$1"
+OUTFILE="$2"
+shift 2
+
+find "$@" -type d -print0 | xargs -0r chmod 755 --
+find "$@" -type f -print0 | xargs -0r chmod 644 --
+find "$@" -print0 -print0 | xargs -0r touch --no-dereference --date="$TIMESTAMP" --
+find "$@" | sort | zip -X -@ "$OUTFILE"
+
+
+
